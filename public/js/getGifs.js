612acc890b10482e20ee93b876cab5bc69c3a689
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-03-09 13:10:36 -0600

    added JS that dynamically grabs image links from the GCP bucket

---
+++ public/js/getGifs.js
@@ -0,0 +1,9 @@
+const getGifs = async () => {
+    const response = await fetch('https://storage.googleapis.com/storage/v1/b/gif-gallery/o/')
+    const { items } = await response.json()
+
+    items.forEach(item => IMAGE_LINKS.push(item.selfLink))
+    console.log(IMAGE_LINKS)
+}
+
+window.addEventListener('load', getGifs)
