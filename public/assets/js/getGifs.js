8d3225c686633bd81a6666fffc240d3bbb8506ea
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-03-12 19:54:44 -0600

    :minus: removed stray console.log and .gitattributes which is no longer needed. Also removed Bootstrap JS cdn

--- public/assets/js/getGifs.js
+++ public/assets/js/getGifs.js
@@ -1,10 +1,12 @@
 const getGifs = async () => {
+    // get all gifs from the bucket
     const response = await fetch('https://storage.googleapis.com/storage/v1/b/gif-gallery/o/')
     const { items } = await response.json()
 
+    // add all links to the IMAGE_LINKS array
     items.forEach(item => IMAGE_LINKS.push(item.mediaLink))
-    console.log(IMAGE_LINKS)
 
+    // display the gifs
     displayGifs(IMAGE_LINKS)
 }
 
